79af5eb7789fac9e16a9ca7cccfdf7f756e04f12
yatinkumar01 <yatins9152015@gmail.com>
2022-04-27 21:18:06 +0530

    Solution to Bubble Sort Algorithm

--- BubbleSort.js
+++ BubbleSort.js
@@ -1,1 +1,20 @@
-console.log("Bubbly Sort")
+var arr=[9,7,4,3,1]
+
+for(var i=0; i<arr.length; i++)
+{
+  for(var j=0; j<arr.length-1-i; j++)
+  {
+    if(arr[j]>arr[j+1])
+    {
+      swap(j,j+1,arr)
+    }
+  }
+}
+console.log(arr.join(" "));
+
+  function swap(i,j,arr)
+  {
+    var temp= arr[i]
+    arr[i]= arr[j]
+    arr[j]= temp
+    }
