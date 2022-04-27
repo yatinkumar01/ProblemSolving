6a413192c7011b3eff94379a2a9857eb9e33f29e
yatinkumar01 <yatins9152015@gmail.com>
2022-04-27 20:22:55 +0530

    Solution to the Selection Sort Algorithm

--- SelectionSort.js
+++ SelectionSort.js
@@ -1,1 +1,20 @@
-console.log("hello")
+var arr = [1,7,4,3,9,2]
+
+for(var i=0; i<arr.length-1; i++)
+{
+  for(var j=i+1; j<arr.length; j++)
+  {
+    if(arr[i]>arr[j])
+    {
+      swap(i,j,arr)
+    }
+  }
+}
+
+console.log(arr.join(" "));
+
+function swap(i,j,arr){
+  var temp = arr[i]
+  arr[i] = arr[j]
+  arr[j] = temp
+}
